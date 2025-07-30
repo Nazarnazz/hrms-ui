af45c1f30cce026d28fcd1f6428f8018ecccbb7f
nazar <nazarnazz06@gmail.com>
2025-07-30 16:30:52 +0800

    perbaikan sidebar, kurang footer

--- src/app/components/menu-items/layout.js
+++ src/app/components/menu-items/layout.js
@@ -1,10 +1,34 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "@/app/components/menu-items/navbar";
 import Sidebar from "@/app/components/menu-items/sidebar";
+import { usePathname } from "next/navigation";
 
 export default function Layout({ children }) {
+  const pathname = usePathname();
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const handleContentClick = () => {
+    if (isSidebarOpen) {
+      setIsSidebarOpen(false);
+    }
+  };
+  // 1. Ambil dari localStorage saat mount
+  useEffect(() => {
+    const stored = localStorage.getItem("sidebarOpen");
+    if (stored === "true") {
+      setIsSidebarOpen(true);
+    }
+  }, []);
+
+  // 2. Simpan ke localStorage setiap kali isOpen berubah
+  useEffect(() => {
+    localStorage.setItem("sidebarOpen", isSidebarOpen);
+  }, [isSidebarOpen]);
+
+  // 3. Tutup sidebar saat berpindah halaman
+  useEffect(() => {
+    setIsSidebarOpen(false);
+  }, [pathname]);
 
   const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
 
@@ -14,7 +38,9 @@ export default function Layout({ children }) {
       <Sidebar isOpen={isSidebarOpen} />
 
       {/* Konten utama: geser jika sidebar terbuka */}
-      <div className={`transition-all duration-300 ${isSidebarOpen ? "sm:ml-64" : "ml-0"} p-4`}>{children}</div>
+      <div onClick={handleContentClick} className={`transition-all duration-300 ${isSidebarOpen ? "sm:ml-64" : "ml-0"} p-4`}>
+        {children}
+      </div>
     </>
   );
 }
