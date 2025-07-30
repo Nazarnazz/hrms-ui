f04336d259013f1690b72af16d6775a650644759
nazar <nazarnazz06@gmail.com>
2025-07-30 16:58:24 +0800

    footer

--- src/app/components/menu-items/layout.js
+++ src/app/components/menu-items/layout.js
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import Navbar from "@/app/components/menu-items/navbar";
 import Sidebar from "@/app/components/menu-items/sidebar";
+import Footer from "@/app/components/menu-items/footer";
 import { usePathname } from "next/navigation";
 
 export default function Layout({ children }) {
@@ -40,6 +41,7 @@ export default function Layout({ children }) {
       {/* Konten utama: geser jika sidebar terbuka */}
       <div onClick={handleContentClick} className={`transition-all duration-300 ${isSidebarOpen ? "sm:ml-64" : "ml-0"} p-4`}>
         {children}
+        <Footer />
       </div>
     </>
   );
